90be236a2d5aa21961df5c2df80d3f42846e9b4b
Devin Fisher <dev.fisher@gmail.com>
2016-09-28 17:30:37 -0600

    redid report to not auto generate

--- api/report.py
+++ api/report.py
@@ -4,18 +4,19 @@ import falcon
 from league import GameList
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-
+from lib.util.static_vals import REPORTS_DIR
 games_list = GameList()
 
 
 class Report(object):
     def on_get(self, req, resp, league_id, tournament_id, bracket_id, match_id):
-        games = None
+        path = os.path.join(REPORTS_DIR, match_id+".zip")
 
-        if not games:
+        if not os.path.isfile(path):
             raise falcon.HTTPNotFound()
 
-        resp.data = ""
+        resp.stream = open(path, 'rb')
+        resp.stream_len = os.path.getsize(path)
         resp.content_type = "application/zip"
         resp.append_header('Content-Disposition', 'attachment; filename=complete_report.zip')
 
