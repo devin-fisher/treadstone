3b048b0916f6831f6adf72b1e5249fda8f245a4d
Devin Fisher <dev.fisher@gmail.com>
2016-12-03 20:52:31 -0700

    changed command to work better with video editors needs

--- lib/game_analysis/video_download_template.py
+++ lib/game_analysis/video_download_template.py
@@ -1,10 +1,12 @@
-standard_snipt = "echo Downloading %(game_name)s:\r\nyoutube-dl -f bestvideo+bestaudio" \
-                 " -o '%(game_name)s_full_game_video.mp4'  %(youtube_url)s"
-
+standard_command = "echo Downloading %(game_name)s:\r\nyoutube-dl "\
+"-f bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio "\
+"--merge-output-format mp4 " \
+"-o '%(game_name)s_full_game_video.mp4' " \
+"%(youtube_url)s"
 
 def video_download_batch_file(game_list):
     rtn = "@echo off\r\n\r\n"
     for game_info in game_list:
-        rtn += standard_snipt % game_info
+        rtn += standard_command % game_info
         rtn += "\r\n"
     return rtn
