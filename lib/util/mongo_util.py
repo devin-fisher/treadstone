d2458fdfb8309f35a0d6c036eb55b9daa1748dd5
Devin Fisher <dev.fisher@gmail.com>
2017-01-15 22:21:40 -0700

    added main to a util

--- lib/util/mongo_util.py
+++ lib/util/mongo_util.py
@@ -2,3 +2,6 @@ import hashlib
 
 def mongodb_id_convert(id):
     return hashlib.md5(id).hexdigest()[:24]
+
+if __name__ == "__main__":
+    print(mongodb_id_convert('95ada0b9-bece-48cc-bd48-d9ee5bde901e'))
